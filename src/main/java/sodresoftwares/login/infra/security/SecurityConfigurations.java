5e8bac3cb27189e057fc8ae4a78b69f7df4296c2
daniel-sd03 <dsodre2003@gmail.com>
2026-07-10 17:21:48 -0300

    feat(Swagge): integrate Swagger/OpenAPI for API documentation

--- src/main/java/sodresoftwares/login/infra/security/SecurityConfigurations.java
+++ src/main/java/sodresoftwares/login/infra/security/SecurityConfigurations.java
@@ -36,6 +36,8 @@ public class SecurityConfigurations {
 						.requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
 						.requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
 						.requestMatchers("/oauth2/**", "/login/oauth2/**").permitAll()
+						.requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+						.requestMatchers("/error").permitAll()
 						.requestMatchers(HttpMethod.POST, "/home").hasRole("ADMIN")
 						.anyRequest().authenticated()
 				)
